d35842579f99846e5ea3fa629587971dbe9483dc
marafrass <55090657+marafrass@users.noreply.github.com>
2021-02-08 19:38:14 -0500

    Added Prototype 1 project

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject == player) 
+        {
+            Vector3 force = new Vector3(0,100,0);
+            this.GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Raycast.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+
+    public Camera maincamera;
+    public GameObject prefab;
+
+    int delay = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+
+            delay += 1;
+            if (delay >= 15)
+            {
+
+
+                Debug.Log("Clicked");
+
+                Ray clickray = maincamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit result;
+                if (Physics.Raycast(clickray, out result))
+                {
+                    GameObject.Instantiate(prefab, result.point, Quaternion.identity);
+
+                }
+                delay = 0;
+
+            }   
+        
+        }
+        else
+        {
+            delay = 15;
+        }
+    }
+}
